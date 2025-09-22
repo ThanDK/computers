43b8b0bd0fb2812bafb12be460b401a32ca9a08a
thanadon <thanadon06452@gmail.com>
2025-09-22 17:53:59 +0700

    Re(UserUi): Add ConfigApi

--- User-ui/src/api/api.jsx
+++ User-ui/src/api/api.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../services/apiConfig';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', 
+    baseURL: API_BASE_URL, 
     headers: {
         'Content-Type': 'application/json',
     },
--- User-ui/src/api/axiosConfig.jsx
+++ User-ui/src/api/axiosConfig.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../services/apiConfig';
 
 const api = axios.create({
 
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+    baseURL: API_BASE_URL,
 });
 
 api.interceptors.request.use(config => {
